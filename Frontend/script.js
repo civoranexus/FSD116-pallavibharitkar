34fa38479846e496884df9d636f49d8dee8ab7b4
pallavibharitkar <pallavibharitkar11@gmail.com>
2026-01-15 09:19:18 +0530

    Added input validation using javascript.

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,10 @@
-function loginUser(){
-    alert ("Login button clicked");
+function loginUser() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        alert("Please enter username and password");
+    } else {
+        alert("Login successful (frontend check)");
+    }
 }
